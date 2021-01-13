943cdf87095eb5ba6cb370d998d08e0d07fc0ab5
@milesibastos <antonio@milesibastos.com>
2021-01-13 16:36:03 -0300

    yarn workspace functions add -D firebase-functions-test jest ts-jest @types/jest eslint-plugin-jest

--- packages/functions/.eslintrc.js
+++ packages/functions/.eslintrc.js
@@ -1,25 +1,30 @@
 module.exports = {
   env: {
-    es6: true,
-    node: true,
+    "es6": true,
+    "node": true,
+    "jest/globals": true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'google',
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:jest/recommended",
+    "google",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ['tsconfig.json', 'tsconfig.dev.json'],
-    sourceType: 'module',
+    project: [
+      "packages/functions/tsconfig.json",
+      "packages/functions/tsconfig.dev.json",
+    ],
+    sourceType: "module",
   },
   ignorePatterns: [
-    '/lib/**/*', // Ignore built files.
+    "/lib/**/*", // Ignore built files.
   ],
-  plugins: ['@typescript-eslint', 'import'],
+  plugins: ["@typescript-eslint", "import", "jest"],
   rules: {
-    quotes: ['error', 'double'],
+    quotes: ["error", "double"],
   },
 };
