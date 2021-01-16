8b05758e200d36580ee428ac3a6cff069af16773
@milesibastos <antonio@milesibastos.com>
2021-01-16 10:39:52 -0300

    chore(webpack): scriptLoading: 'defer'
    Modern browsers support non blocking javascript loading ('defer') to improve the page startup performance.
    
    https://github.com/jantimon/html-webpack-plugin#options

--- packages/web/config/webpack.config.js
+++ packages/web/config/webpack.config.js
@@ -561,6 +561,7 @@ module.exports = function (webpackEnv) {
           {
             inject: true,
             template: paths.appHtml,
+            scriptLoading: 'defer',
           },
           isEnvProduction
             ? {
